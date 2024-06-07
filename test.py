ce03e3c79d2375f4d8f96c282246669f31ebf9e3
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2024-06-07 19:26:41 +0530

    added
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

---
+++ test.py
@@ -0,0 +1,13 @@
+# Experiment
+
+my_list_a = [1, 5, "Hello", -1.4, "how", 103, "are", "you", "you"]
+# print(my_list_a.pop(7))
+
+my_list_a.remove(my_list_a.pop(7))
+
+# my_list_a.remove("you")
+# my_list_a.remove(my_list_a.pop(7))
+
+print(my_list_a)
+
+
